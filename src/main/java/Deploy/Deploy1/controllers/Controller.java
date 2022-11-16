abec328f6380d71d6809f846a01b5a56dcb5d5cb
RaimondnicusorBulai <Condivisionedatiraimond@gmail.com>
2022-11-16 03:33:16 +0100

    modify 20221115

--- src/main/java/Deploy/Deploy1/controllers/Controller.java
+++ src/main/java/Deploy/Deploy1/controllers/Controller.java
@@ -1,14 +1,16 @@
 package Deploy.Deploy1.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+    @Value("${devName}")
+    String devName;
 
     @GetMapping
     public String getProperty(){
-        String devName = "Raimond Nicusor Bulai";
         return devName;
     }
 }
